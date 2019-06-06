2640d2a04465ec1390f658691edfb73676e94cc7
tirthrajrao <tirthrajbarot2394@gmail.com>
2019-06-06 10:30:13 +0530

    Updates

---
+++ precache-manifest.eed1c38779e94d92cf10d48a654a1047.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73689de05814193d5193c2a21ac7696c",
+    "url": "/Stock/index.html"
+  },
+  {
+    "revision": "7edfa562eef34ba8d081",
+    "url": "/Stock/static/css/main.599ddb91.chunk.css"
+  },
+  {
+    "revision": "e9052ec22d9d06895c96",
+    "url": "/Stock/static/js/2.cc40ad66.chunk.js"
+  },
+  {
+    "revision": "7edfa562eef34ba8d081",
+    "url": "/Stock/static/js/main.ec6aff97.chunk.js"
+  },
+  {
+    "revision": "1fc3f6afd727c12e6603",
+    "url": "/Stock/static/js/runtime~main.a71eb1a4.js"
+  },
+  {
+    "revision": "de08db182391e4f66c7833a61bbdba82",
+    "url": "/Stock/static/media/bgimage.de08db18.jpg"
+  }
+]);
