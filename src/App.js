cc786e8ca700294c380a21158e150257d3ad90c4
tirthrajrao <tirthrajbarot2394@gmail.com>
2019-06-09 13:55:58 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Login from './components/Login';
+import Companylist from './components/Company-list';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = localStorage.getItem('email1');
+  }
+  render() {
+    if(!this.state){
+            return (
+                <div>
+                <Login />
+                </div>
+                );
+        }else{
+            return (
+                <div>
+                <Companylist />
+                </div>
+                );
+        }
+  }
+}
+
+export default App;
