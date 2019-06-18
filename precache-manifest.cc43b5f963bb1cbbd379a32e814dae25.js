f5168be7f4cb4a3e07a319b992faacdb74975175
tirthrajrao <tirthrajbarot2394@gmail.com>
2019-06-18 10:59:27 +0530

    Updates

---
+++ precache-manifest.cc43b5f963bb1cbbd379a32e814dae25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee8dd6db4ad20a11c41df408c2cfdd6",
+    "url": "/Stock-viewer/index.html"
+  },
+  {
+    "revision": "64bfa5089afd5cffd7ee",
+    "url": "/Stock-viewer/static/css/main.ab8bbb04.chunk.css"
+  },
+  {
+    "revision": "f05ec4cfbf657d3e05c9",
+    "url": "/Stock-viewer/static/js/2.e6b55997.chunk.js"
+  },
+  {
+    "revision": "64bfa5089afd5cffd7ee",
+    "url": "/Stock-viewer/static/js/main.8f1bca28.chunk.js"
+  },
+  {
+    "revision": "71fbe239509ba0cfbaa1",
+    "url": "/Stock-viewer/static/js/runtime~main.c8d0de04.js"
+  },
+  {
+    "revision": "de08db182391e4f66c7833a61bbdba82",
+    "url": "/Stock-viewer/static/media/bgimage.de08db18.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Stock-viewer/precache-manifest.3a7f4bc886dee5a17cecfd07573e2ef4.js"
+  "/Stock-viewer/precache-manifest.cc43b5f963bb1cbbd379a32e814dae25.js"
 );
 
 self.addEventListener('message', (event) => {
